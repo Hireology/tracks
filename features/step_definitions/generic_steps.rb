31f961c512bb3bcf69c80de6321110e28b8e135f
Dan Rice <drice@slinky>
2013-03-02 04:21:42 -0500

    Eliminate a race condition in the flash message step definition

--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -17,10 +17,7 @@ end
 
 Then /^I should see an error flash message saying "([^"]*)"$/ do |message|
   xpath = "//div[@id='message_holder']/h4[@id='flash']"
-  page.should have_xpath(xpath, :visible => true)
-  
-  text = page.find(:xpath, xpath).text
-  text.should == message
+  page.should have_xpath(xpath, :visible => true, :text => message)
 end
 
 Then /^I should see "([^"]*)" $/ do |text|
